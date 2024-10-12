d8912ea2025818b1ba4ead879fb1f762664711d9
ANVod <157865931+ANVod@users.noreply.github.com>
2024-10-12 23:40:18 +0300

    Fixit off
    Исправлено отображение корзины и личного кабинета. А так же выход из приложения.

--- flower_delivery/reviews/urls.py
+++ flower_delivery/reviews/urls.py
@@ -6,5 +6,4 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('add/<int:flower_id>/', views.add_review, name='add_review'),
-    path('add/', views.add_review, name='add_review'),
 ]
