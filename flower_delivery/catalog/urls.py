ad27f83c76ebb082631169b2f378e4e832c86b2d
ANVod <157865931+ANVod@users.noreply.github.com>
2024-10-12 20:59:14 +0300

    fixit
    Первая страница открылась. Присутствует только ссылка на каталог но не открывается.

--- flower_delivery/catalog/urls.py
+++ flower_delivery/catalog/urls.py
@@ -6,4 +6,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.catalog_list, name='catalog_list'),
     path('flower/<int:flower_id>/', views.flower_detail, name='flower_detail'),
+    path('add_review/<int:flower_id>/', views.add_review, name='add_review'),
 ]
--- flower_delivery/catalog/views.py
+++ flower_delivery/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Flower
+from reviews.forms import ReviewForm
 
 def catalog_list(request):
     flowers = Flower.objects.all() # Получение всех цветов из базы
@@ -11,5 +12,16 @@ def flower_detail(request, flower_id):
     print('Flower Name:', flower.name)
     return render(request, 'catalog/flower_detail.html', {'flower': flower})
 
-
+def add_review(request, flower_id):
+    flower = get_object_or_404(Flower, id=flower_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.flower = flower
+            review.save()
+            return redirect('catalog:flower_detail', flower_id=flower_id)
+    else:
+        form = ReviewForm()
+    return render(request, 'catalog/add_review.html', {'form': form, 'flower': flower})
 # Create your views here.
