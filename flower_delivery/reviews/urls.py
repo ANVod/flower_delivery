e9bf30315169e46fc7bef0889021ff3c0322884b
ANVod <157865931+ANVod@users.noreply.github.com>
2024-10-12 20:27:50 +0300

    Fixit
    Страницы так и не открываются

--- flower_delivery/reviews/urls.py
+++ flower_delivery/reviews/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.review_list, name='review_list'),
-    path('add/<int:flower_id>/', views.add_review, name='add_review'),
+    path('reviews/add/<int:flower_id>/', views.add_review, name='add_review'),
 ]
