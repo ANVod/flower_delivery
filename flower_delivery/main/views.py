8eb341209807fb66c17de1d15d705ef21b35c8d7
ANVod <157865931+ANVod@users.noreply.github.com>
2024-10-24 23:39:19 +0300

    add carisell
    Добавлена карусель на главную страницу.

--- flower_delivery/main/views.py
+++ flower_delivery/main/views.py
@@ -4,4 +4,4 @@ from catalog.models import Flower
 def index(request):
     # Фильтруем цветы по критерию распродажи или новинок сезона
     flowers = Flower.objects.filter(is_on_sale=True)  # Добавьте поле в модели для распродаж или новинок
-    return render(request, 'main/index.html', {'flowers': flowers})
+    return render(request, 'index.html', {'flowers': flowers})
